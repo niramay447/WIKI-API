f0905bb23c592494901091fc1c0f2b25bb020e98
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-07-13 15:36:34 +0530

    Fetch all articles

--- app.js
+++ app.js
@@ -19,6 +19,17 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+
+app.get("/articles",function(req,res){
+    Article.find(function(err, foundArticles){
+        if(!err){
+            res.send(foundArticles);
+        }
+        else {
+            res.send(err);
+        }
+    });
+})
 mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser:true});
 app.listen(3000, function(){
     console.log("Server started on per 3000");
