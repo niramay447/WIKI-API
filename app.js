b2a462a8fd1342242b5026067853607bc03882a4
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-07-14 11:02:53 +0530

    Chained route handlers using express

--- app.js
+++ app.js
@@ -19,36 +19,39 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-
-app.get("/articles",function(req,res){
-    Article.find(function(err, foundArticles){
-        if(!err){
-            res.send(foundArticles);
-        }
-        else {
-            res.send(err);
-        }
-    });
-});
-
-app.post("/articles",function(req,res){
-    console.log(req.body.title);
-    console.log(req.body.content);
-
-    const newArticle = new Article({
-        title:req.body.title,
-        content : req.body.content
-    });
-
-    newArticle.save(function(err){
-        if(!err){
-            res.send("Successfully added a new article");
-        } else {
-            res.send(err);
-        }
-    })
-});
-app.delete("/articles", function(req,res){
+app.route("/articles").get(
+    function(req,res){
+        Article.find(function(err, foundArticles){
+            if(!err){
+                res.send(foundArticles);
+            }
+            else {
+                res.send(err);
+            }
+        });
+    }
+)
+.post(
+    function(req,res){
+        console.log(req.body.title);
+        console.log(req.body.content);
+    
+        const newArticle = new Article({
+            title:req.body.title,
+            content : req.body.content
+        });
+    
+        newArticle.save(function(err){
+            if(!err){
+                res.send("Successfully added a new article");
+            } else {
+                res.send(err);
+            }
+        })
+    }
+)
+.delete(
+    function(req,res){
     Article.deleteMany(function(err){
         if(!err){
             res.send("Successfully deleted all articles");
@@ -56,8 +59,8 @@ app.delete("/articles", function(req,res){
         else{
             res.send(err);
         }
-    })
-})
+    });
+});
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser:true});
 app.listen(3000, function(){
